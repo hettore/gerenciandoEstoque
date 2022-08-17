ca396c84c2de47b027565a3316b1e20244e0ab73
Hettore Eduardo <duh.barretoo@gmail.com>
2022-08-17 10:12:14 -0300

    Exercício sobre construtores, this, sobrecarga e encapsulamento.

---
+++ src/application/Program.java
@@ -0,0 +1,57 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US); //Para resolver a questão do uso do .
+		Scanner sc = new Scanner(System.in); //Para conseguir ler as informações.Lembrar de fechar sc.close()
+		
+		System.out.println("Enter product data: "); //
+		System.out.print("Name:");
+		String name = sc.nextLine(); //Leitura do nome do produto e ja armazena na variável name
+		System.out.print("Price: ");
+		double price = sc.nextDouble(); //Leitura do valor e armazena na variável price
+		System.out.print("Quantity in stock: ");
+		int quantity = sc.nextInt(); // Leitura da quantidade e armazena na variável quantity
+		
+		Product product = new Product(name, price, quantity); //Não esqueça de importar a classe.
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+//		product.setName("pc");
+//		System.out.println("Updated name: " + product.getName());
+//		product.setPrice(1200.00);
+//		System.out.println("Updated price: " + product.getPrice());
+		
+		
+//		System.out.println();
+//		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be added in stock: ");
+		quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		sc.close();
+		
+	}
+
+}
---
+++ src/entities/Product.java
@@ -0,0 +1,81 @@
+package entities;
+
+public class Product {
+	
+	//Definindo os atributos da classe Product, quando usamos o encapsulamento os atributos ficam em private e são acessador por metódos get e set.
+	String name;
+	private double price;
+	private int quantity;
+	
+	//Usando construtor
+	
+	public Product() {
+		
+	}
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+		
+	}
+	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+		//this.quantity = 0; //  não é necessario o uso pq por paremetro essa variavel ja é inicializada com 0
+		
+	}
+	
+	//Usando getters e setters
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	//Definindo os métodos
+	//operação para devolver o valor total do estoque
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+	
+	//Operação para adicionar produtos ao estoque.
+	//Usamos o this para referenciar o atributo da classe e não o parametro da função.
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	//Operação para remover produto do estoque.
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+	
+	//Operação para converter o objeto para string, sobreposição 
+	public String toString() {
+		return name
+				+ ", $"
+				+ String.format("%.2f", price)
+				+ ", "
+				+ quantity
+				+ " units, Total: $"
+				+ String.format("%.2f", totalValueInStock());
+				
+				
+	}
+}
